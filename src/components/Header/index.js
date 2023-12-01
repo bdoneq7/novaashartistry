00f4729f159ce4e016a41afe242ae3fde03d7d64
Brian Done <briguy2078@gmail.com>
2023-12-01 16:48:41 -0500

    updated logo image format

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../Nav";
-import logo from "../../assets/images/logocardsmall.png";
+import logo from "../../assets/images/logocardsmall.jpg";
 
 function Header(props) {
 	const { currentTab, setCurrentTab } = props;
