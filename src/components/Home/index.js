2f2b4a1172d9d04a96da11bf6abd801f0c6f0983
Brian Done <briguy2078@gmail.com>
2023-12-01 18:02:16 -0500

    add divs to home

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,14 +7,18 @@ function Home(props) {
     <section>
       <h1 className="mx-2" id="about">Nova Ash Artistry Home</h1>
 
-      <p>
-        Nova Ash Artistry provides high quality hair styling by Summer Done.
+      <div className="leftsection">
+        <p>
+          Nova Ash Artistry provides high quality hair styling by Summer Done.
         </p>
+      </div>
       
-      <div className="my-2 mx-2">
+      
+      <div className="rightsection">
       <img src={hair} className="my-2" style={{ width: "50%", height: "50%" }} alt="" title="" />
         
       </div>
+      
     </section>
   );
 
