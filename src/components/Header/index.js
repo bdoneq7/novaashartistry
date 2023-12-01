e3840a5163db462eed86f9ea196717fa9fb2b081
Brian Done <briguy2078@gmail.com>
2023-12-01 16:51:07 -0500

    updated logo image size

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Nav from "../Nav";
-import logo from "../../assets/images/logocardsmall.jpg";
+import logo from "../../assets/images/logocardsmaller.jpg";
 
 function Header(props) {
 	const { currentTab, setCurrentTab } = props;
