bafbc29b290dbbcde7e9fb11697ea163a4bf19e9
Brian Done <briguy2078@gmail.com>
2023-12-05 22:20:11 -0500

    logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -21,7 +21,7 @@ function Header(props) {
 			</div>
 
 			<div class="flowerdiv">
-			<img src={flower} style={{ width: "20%", height: "20%" }} alt="Nova Ash Artistry" title="Nova Ash Artistry" />
+			<img src={flower} style={{ width: "40%", height: "40%" }} alt="Nova Ash Artistry" title="Nova Ash Artistry" />
 			</div>
 
 		</header>
