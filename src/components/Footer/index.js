d0280265281ab4d85af6006ed0a6b48871aa767b
Brian Done <briguy2078@gmail.com>
2023-12-06 18:51:03 -0500

    updated footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,37 @@ function Footer() {
 	return (
 		<footer className="footer">
 			<br/>
-			Contact Info | Fake Address | Instagram Logo | Hours
+
+			<div class="footersection">
+				SCHEDULE YOUR<br/>
+				RESERVATION<br/>
+				<br/>
+				813.541.3969<br/>
+				Contact Us Today
+			</div>
+			
+			<div class="footersection">
+				123 Sassennch Dr.<br/>
+				Lallybroch, FL 12345
+			</div>
+			
+			<div class="footersection">
+				Instagram
+			</div>
+			
+			<div class="footersection">
+				OUR HOURS<br/>
+				<br/>
+				Monday: Closed<br/>
+				Tuesday: 10 am to 8 pm<br/>
+				Wednesday: 10 am to 8 pm<br/>
+				Thursday: 10 am to 9 pm<br/>
+				Friday: 10 am to 7 pm<br/>
+				Saturday: 9 am to 6 pm<br/>
+				Sunday: Closed
+			</div>
+			
+		
 		</footer>
 	);
 }
