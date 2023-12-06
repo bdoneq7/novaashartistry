a71aca5f3bbf16bc6112b95804be079744ecfee1
Brian Done <briguy2078@gmail.com>
2023-12-05 22:05:13 -0500

    updated portfolio

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -2,6 +2,28 @@ import React from 'react';
 import project1 from "../../assets/images/project1.jpg";
 import project2 from "../../assets/images/project2.jpg";
 import project3 from "../../assets/images/project3.jpg";
+import project4 from "../../assets/images/project4.jpg";
+import project5 from "../../assets/images/project5.jpg";
+import project6 from "../../assets/images/project6.jpg";
+import project7 from "../../assets/images/project7.jpg";
+import project8 from "../../assets/images/project8.jpg";
+import project9 from "../../assets/images/project9.jpg";
+import project10 from "../../assets/images/project10.jpg";
+import project11 from "../../assets/images/project11.jpg";
+import project12 from "../../assets/images/project12.jpg";
+import project13 from "../../assets/images/project13.jpg";
+import project14 from "../../assets/images/project14.jpg";
+import project15 from "../../assets/images/project15.jpg";
+import project16 from "../../assets/images/project16.jpg";
+import project17 from "../../assets/images/project17.jpg";
+import project18 from "../../assets/images/project18.jpg";
+import project19 from "../../assets/images/project19.jpg";
+import project20 from "../../assets/images/project20.jpg";
+import project21 from "../../assets/images/project21.jpg";
+import project22 from "../../assets/images/project22.jpg";
+import project23 from "../../assets/images/project23.jpg";
+import project24 from "../../assets/images/project24.jpg";
+import project25 from "../../assets/images/project25.jpg";
 
 
 function Portfolio(props) {
@@ -29,6 +51,138 @@ function Portfolio(props) {
           <img src={project3} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
           <br/>
           Project 3
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project4} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 4
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project5} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 5
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project6} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 6
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project7} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 7
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project8} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 8
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project9} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 9
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project10} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 10
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project11} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 11
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project12} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 12
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project13} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 13
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project14} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 14
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project15} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 15
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project16} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 16
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project17} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 17
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project18} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 18
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project19} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 19
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project20} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 20
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project21} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 21
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project22} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 22
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project23} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 23
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project24} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 24
+        </div>
+
+        <div className="sectionpicdarkport">
+          <img src={project25} style={{ width: "80%", height: "80%" }} alt="Education" title="Education" />
+          <br/>
+          Project 25
         </div>
 
         <div className="sectionpicdarkportsliver">
