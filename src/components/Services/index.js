35d1ffd14c32f3f67c23aa93cc51ea5d90b37a31
Brian Done <briguy2078@gmail.com>
2023-12-05 21:20:15 -0500

    updated services

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -20,20 +20,25 @@ function Services(props) {
         <h3 class="moveright">HAIR SERVICES</h3>
         
         <div className="servicessectiondarktype">
-        <ul>
-          <li>&#8226; Consulation                     FREE</li>
-          <li>&#8226; Shampoo & Shear Cut             $45</li>
-          <li>&#8226; Shampoo, Shear Cut, Style       $60</li>
-          <li>&#8226; Clipper Cut                     $30</li>
-          <li>&#8226; Combination Cut                 $50</li>
-          <li>&#8226; Child Cut 12 and Under          $20</li>
-          <li>&#8226; Shampoo and Blow dry Only       $40</li>
-          <li>&#8226; Deep Conditioner add-on         $10</li>
-      </ul>
+        &#8226; Consulation                     
+        &#8226; Shampoo & Shear Cut             
+        &#8226; Shampoo, Shear Cut, Style       
+        &#8226; Clipper Cut                     
+        &#8226; Combination Cut                 
+        &#8226; Child Cut 12 and Under          
+        &#8226; Shampoo and Blow dry Only       
+        &#8226; Deep Conditioner add-on         
       </div>
 
       <div className="servicessectiondarkprice">
-        
+        &#8226; FREE<br/>
+        &#8226; $45<br/>
+        &#8226; $60<br/>
+        &#8226; $30<br/>
+        &#8226; $50<br/>
+        &#8226; $20<br/>
+        &#8226; $40<br/>
+        &#8226; $10<br/>
       
       </div>
 
