0b520c8d70edd67339bdacc34399b1283a3c58ce
Brian Done <briguy2078@gmail.com>
2023-12-07 16:27:57 -0500

    updated about

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import homeback from "../../assets/images/homeback.jpg";
+import aboutus from "../../assets/images/aboutus.png";
 
 
 function Home(props) {
@@ -12,7 +13,28 @@ function Home(props) {
       
       
       <div className="aboutussection">
-      About Us Section Info
+
+      <div className="mainlinktitle">
+        <h1 id="about">ABOUT US</h1>
+        </div>
+
+        <div className="leftsectionpicdark">
+          <img src={aboutus} style={{ width: "100%", height: "100%" }} alt="About Us" title="About Us" />
+        </div>
+
+        <div className="rightsectiondark">
+          <br/>
+        <h3>BEHIND NOVA ASH</h3>
+        
+        <br/>
+        Nova Ash was born out of the desire to be meaningful in the approach to cosmetology. One of the meanings of NOVA is 'new' and
+        one of the origins of ASH is from the name 'Asher' or 'happy.' The Ash tree typically supports rich and diverse ground flora,
+        with leaving retaining more nutrients, and improve the soil by raising the pH level.<br/> 
+        <br/>
+        Like the ash trees, Nova Ash Artistry strives to maintain and improve the health and strength of our guests hair and skin, 
+        leaving them feeling beautiful, inside and out.
+        </div>
+
       </div>
 
     </section>
