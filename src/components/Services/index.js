15e1a98e1a9c32f8c05df0f16c2244b340f3f18a
Brian Done <briguy2078@gmail.com>
2023-12-18 00:08:21 -0500

    updated services

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -100,25 +100,25 @@ function Services(props) {
         <h3 class="moveright">COLOR SERVICES</h3>
         
         <div className="servicessectiondarktype">
-        &#8226; Consulation<br/>                     
-        &#8226; Shampoo & Shear Cut<br/>           
-        &#8226; Shampoo, Shear Cut, Style<br/>      
-        &#8226; Clipper Cut<br/>                    
-        &#8226; Combination Cut<br/>                 
-        &#8226; Child Cut 12 and Under<br/>        
-        &#8226; Shampoo and Blow dry Only<br/>      
-        &#8226; Deep Conditioner add-on<br/>        
+        &#8226; Consulation/Patch/Strand Test<br/>                     
+        &#8226; Single Process Color<br/>           
+        &#8226; Double Process Color<br/>      
+        &#8226; Color & Partial Foil<br/>                    
+        &#8226; Color & Full Foil<br/>                 
+        &#8226; Balayage<br/>        
+        &#8226; Toner Refresh/Glaze<br/>      
+        &#8226; Color Correction<br/>        
       </div>
 
       <div className="servicessectiondarkprice">
         FREE<br/>
-        $45<br/>
-        $60<br/>
-        $30<br/>
-        $50<br/>
-        $20<br/>
-        $40<br/>
-        $10<br/>
+        $75+<br/>
+        $150+<br/>
+        $160+<br/>
+        $175+<br/>
+        $200+<br/>
+        $50+<br/>
+        $250+<br/>
       
       </div>
 
@@ -134,34 +134,43 @@ function Services(props) {
         <div className="servicessectiondarktype">
         
         &#8226; Consulation<br/>
-        &#8226; Flat Iron<br/>
-        &#8226; Perm<br/>
-        &#8226; Specialty Perm<br/>
-        &#8226; Curl Reformation<br/>
-        &#8226; Partial Perm<br/>
-        &#8226; Keratin Treatment<br/>
-        &#8226; Silk Press<br/>
-        &#8226; Custom Foil<br/>
-        &#8226; Balayage<br/>
-        &#8226; Updo/long Formal Style<br/>
-        &#8226; Extensions<br/>
-        
+        &#8226; Brow Wax<br/>
+        &#8226; Brow Tweeze<br/>
+        &#8226; Eyebrow or eyelash tint<br/>
+        &#8226; Brow wax & tint<br/>
+        &#8226; Brow Lamination<br/>
+        &#8226; Brow Lamination with tint<br/>
+        &#8226; Full Face Wax (including brows)<br/>
+        &#8226; Side of Face Wax<br/>
+        &#8226; Lip wax<br/>
+        &#8226; Chin wax<br/>
+        &#8226; 1/2 Arm wax<br/>
+        &#8226; Full Arm wax<br/>
+        &#8226; Lower Leg (knee below)<br/>
+        &#8226; Full Leg<br/>
+        &#8226; Basic Facial (30 minutes)<br/>
+        &#8226; Spa Facial (60 minutes)<br/>
       
       </div>
 
       <div className="servicessectiondarkprice">
       FREE<br/>
-      $45<br/>
-      $65+<br/>
-      $75+<br/>
-      $75+<br/>
-      $55<br/>
-      $200+<br/>
-      $150+<br/>
-      $80-$200+<br/>
-      $175+<br/>
-      $85+<br/>
-      $300+
+      $20<br/>
+      $25<br/>
+      $20<br/>
+      $35<br/>
+      $60<br/>
+      $75<br/>
+      $50<br/>
+      $15<br/>
+      $10<br/>
+      $10<br/>
+      $30<br/>
+      $50<br/>
+      $40<br/>
+      $65<br/>
+      $50<br/>
+      $90<br/>
 
       </div>
         
