84422e6c8eb4b1c574a9546cbaa47067fd0cfef8
Brian Done <briguy2078@gmail.com>
2023-12-17 23:55:50 -0500

    updated services

--- src/components/Services/index.js
+++ src/components/Services/index.js
@@ -87,6 +87,86 @@ function Services(props) {
         
         </div>
 
+
+
+
+
+
+
+
+
+  <div className="servicessectiondark">
+          
+        <h3 class="moveright">COLOR SERVICES</h3>
+        
+        <div className="servicessectiondarktype">
+        &#8226; Consulation<br/>                     
+        &#8226; Shampoo & Shear Cut<br/>           
+        &#8226; Shampoo, Shear Cut, Style<br/>      
+        &#8226; Clipper Cut<br/>                    
+        &#8226; Combination Cut<br/>                 
+        &#8226; Child Cut 12 and Under<br/>        
+        &#8226; Shampoo and Blow dry Only<br/>      
+        &#8226; Deep Conditioner add-on<br/>        
+      </div>
+
+      <div className="servicessectiondarkprice">
+        FREE<br/>
+        $45<br/>
+        $60<br/>
+        $30<br/>
+        $50<br/>
+        $20<br/>
+        $40<br/>
+        $10<br/>
+      
+      </div>
+
+
+
+        </div>
+        
+        
+        <div className="servicessectiondark">
+          
+        <h3 class="moveright">SKIN SERVICES</h3>
+        
+        <div className="servicessectiondarktype">
+        
+        &#8226; Consulation<br/>
+        &#8226; Flat Iron<br/>
+        &#8226; Perm<br/>
+        &#8226; Specialty Perm<br/>
+        &#8226; Curl Reformation<br/>
+        &#8226; Partial Perm<br/>
+        &#8226; Keratin Treatment<br/>
+        &#8226; Silk Press<br/>
+        &#8226; Custom Foil<br/>
+        &#8226; Balayage<br/>
+        &#8226; Updo/long Formal Style<br/>
+        &#8226; Extensions<br/>
+        
+      
+      </div>
+
+      <div className="servicessectiondarkprice">
+      FREE<br/>
+      $45<br/>
+      $65+<br/>
+      $75+<br/>
+      $75+<br/>
+      $55<br/>
+      $200+<br/>
+      $150+<br/>
+      $80-$200+<br/>
+      $175+<br/>
+      $85+<br/>
+      $300+
+
+      </div>
+        
+        </div>      
+
     </section>
   );
 
